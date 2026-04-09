931835b907320a210572bdb8a45f7bbaefa08df9
Mazen Thabet <MazenThabet2@outlook.com>
2026-04-09 22:52:07 +0200

    Prequisites

--- G-NET-26-aAdvanced-02/Program.cs
+++ G-NET-26-aAdvanced-02/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using G_NET_26_aAdvanced_02.ShopMaster;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,23 @@ namespace G_NET_26_aAdvanced_02
     {
         public static void Main(string[] args)
         {
-           
+
+            List<Product> catalog= new()
+            {
+                new Product {Id = 1, Name = "Laptop", Category = "Electronics", Price = 1200, Stock = 10},
+                new Product {Id = 2, Name = "Phone", Category = "Electronics", Price = 800, Stock = 25},
+                new Product {Id = 3, Name = "T-Shirt", Category = "Clothing", Price = 30, Stock = 100},
+                new Product {Id = 4, Name = "Jeans", Category = "Clothing", Price = 60, Stock = 50},
+                new Product {Id = 5, Name = "Chocolate", Category = "Food", Price = 5, Stock = 200},
+                new Product {Id = 6, Name = "Cofee Beans", Category = "Food", Price = 15, Stock = 80},
+                new Product {Id = 7, Name = "C# Book", Category = "Books", Price = 45, Stock = 30},
+                new Product {Id = 8, Name = "Novel", Category = "Books", Price = 20, Stock = 60},
+                new Product {Id = 9, Name = "Headphones", Category = "Electronics", Price = 150, Stock = 15}
+
+            };
+
+
+
         }
     }
 }
