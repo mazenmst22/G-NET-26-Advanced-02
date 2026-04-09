d854bbf3bcd41f07318dd2bbcf176b4641429644
Mazen Thabet <MazenThabet2@outlook.com>
2026-04-09 21:57:44 +0200

    Add project files.

---
+++ G-NET-26-aAdvanced-02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_26_aAdvanced_02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+        }
+    }
+}
